7a21c903824323a5409490eefe0a0de6d3a88702
Anna Staub <info@anna-staub.ch>
2025-02-04 21:24:08 +0100

    JavaScript methoden 'map' und 'forEach'

---
+++ JavaScript/Map/app.js
@@ -0,0 +1,41 @@
+// The map-method iterates over an array, takes each element, does whatever it does to it, returns the result AND pushes it to a new Array
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const doubles = numbers.map(function (num) {
+    return num * 2;
+})
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+const titles = movies.map(function (movie) {
+    return movie.title.toUpperCase();
+})
+
+console.log(doubles);
+console.log(titles);
+
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+const firstNames = fullNames.map(function(name) {
+    return name.first;
+})
+console.log(firstNames);
